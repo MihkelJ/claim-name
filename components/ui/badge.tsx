bfc46f9352e74f1b8661c21912f7977a7891e0ad
mihkel <github.quail204@passinbox.com>
2025-03-19 09:17:00 +0200

    chore: update dependencies

--- components/ui/badge.tsx
+++ components/ui/badge.tsx
@@ -3,20 +3,20 @@ import { cva, type VariantProps } from 'class-variance-authority';
 import * as React from 'react';
 
 const badgeVariants = cva(
-  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2',
+  'inline-flex items-center rounded-md border px-2.5 py-0.5 text-xs font-semibold transition-colors focus:outline-hidden focus:ring-2 focus:ring-ring focus:ring-offset-2',
   {
     variants: {
       variant: {
-        default: 'border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80',
+        default: 'border-transparent bg-primary text-primary-foreground shadow-sm hover:bg-primary/80',
         secondary:
           'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
         destructive:
-          'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
+          'border-transparent bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/80',
         outline: 'text-foreground',
-        info: 'border-transparent bg-blue-500 text-white shadow hover:bg-blue-600',
-        warning: 'border-transparent bg-amber-500 text-white shadow hover:bg-amber-600',
-        error: 'border-transparent bg-red-500 text-white shadow hover:bg-red-600',
-        success: 'border-transparent bg-green-500 text-white shadow hover:bg-green-600',
+        info: 'border-transparent bg-blue-500 text-white shadow-sm hover:bg-blue-600',
+        warning: 'border-transparent bg-amber-500 text-white shadow-sm hover:bg-amber-600',
+        error: 'border-transparent bg-red-500 text-white shadow-sm hover:bg-red-600',
+        success: 'border-transparent bg-green-500 text-white shadow-sm hover:bg-green-600',
       },
     },
     defaultVariants: {
